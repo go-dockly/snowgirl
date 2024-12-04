74374f0fde6b27f67fd498809b4bad6a2376d77a
Sven Woldt <sven.woldt@wellsync.com>
2024-12-04 20:25:50 +0100

    snowgirl: rm: redundant

--- pkg/audio/audio_files.go
+++ pkg/audio/audio_files.go
@@ -1,21 +1,13 @@
 package audio
 
 import (
-	"bytes"
 	_ "embed"
-	"encoding/binary"
-	"errors"
 	"fmt"
 	"io"
-	"net/http"
-	"net/url"
 	"os"
-	"os/signal"
 	"path/filepath"
-	"syscall"
 
 	"github.com/go-audio/wav"
-	"github.com/gordonklaus/portaudio"
 	"github.com/hajimehoshi/go-mp3"
 	"go.uber.org/multierr"
 )
@@ -85,133 +77,4 @@ func loadWAV(filePath string) (frame []float32, err error) {
 		frame[i] = float32(sample / 1 << 15) // Assuming 16-bit PCM
 	}
 	return frame, nil
-}
-
-// https://cloud.google.com/text-to-speech/docs/voices
-func GoogleSay(msg string) (err error) {
-	var url = fmt.Sprintf("http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=%s&tl=%s", url.QueryEscape(msg), "en")
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-	var sig = make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return errors.New("read response")
-	}
-	id, data, err := readChunk(bytes.NewReader(body))
-	if err != nil {
-		return err
-	}
-	if id.String() != "FORM" {
-		return fmt.Errorf("bad file format: %s", id)
-	}
-	_, err = data.Read(id[:])
-	if err != nil {
-		return err
-	}
-	if id.String() != "AIFF" {
-		return fmt.Errorf("bad file format: %s", id)
-	}
-	var c commonChunk
-	var audio io.Reader
-	for {
-		id, chunk, err := readChunk(data)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return err
-		}
-		switch id.String() {
-		case "COMM":
-			if err = binary.Read(chunk, binary.BigEndian, &c); err != nil {
-				fmt.Println("error reading COMM chunk:", err)
-			}
-		case "SSND":
-			_, err = chunk.Seek(8, 1) //ignore offset and block
-			if err != nil {
-				fmt.Println("error seeking SSND chunk:", err)
-			}
-			audio = chunk
-		default:
-			fmt.Printf("ignoring chunk '%s'\n", id)
-		}
-	}
-	//assume 44100 sample rate, mono, 32 bit
-	if err = portaudio.Initialize(); err != nil {
-		return fmt.Errorf("portaudio.Initialize: %w", err)
-	}
-	defer func() {
-		err = multierr.Combine(err, portaudio.Terminate())
-	}()
-	var out = make([]int32, 8192)
-	stream, err := portaudio.OpenDefaultStream(0, 1, 44100, len(out), &out)
-	if err != nil {
-		return err
-	}
-	defer stream.Close()
-	if err = stream.Start(); err != nil {
-		return err
-	}
-	defer func() {
-		err = multierr.Combine(err, stream.Stop())
-	}()
-	for remaining := int(c.NumSamples); remaining > 0; remaining -= len(out) {
-		if len(out) > remaining {
-			out = out[:remaining]
-		}
-		if err := binary.Read(audio, binary.BigEndian, out); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return err
-		}
-		if err = stream.Write(); err != nil {
-			return err
-		}
-		select {
-		case <-sig:
-			return nil
-		default:
-		}
-	}
-	return nil
-}
-
-func readChunk(r readerAtSeeker) (id ID, data *io.SectionReader, err error) {
-	_, err = r.Read(id[:])
-	if err != nil {
-		return
-	}
-	var n int32
-	err = binary.Read(r, binary.BigEndian, &n)
-	if err != nil {
-		return
-	}
-	off, _ := r.Seek(0, 1)
-	data = io.NewSectionReader(r, off, int64(n))
-	_, err = r.Seek(int64(n), 1)
-	return
-}
-
-type readerAtSeeker interface {
-	io.Reader
-	io.ReaderAt
-	io.Seeker
-}
-
-type ID [4]byte
-
-func (id ID) String() string {
-	return string(id[:])
-}
-
-type commonChunk struct {
-	NumChans      int16
-	NumSamples    int32
-	BitsPerSample int16
-	SampleRate    [10]byte
 }
